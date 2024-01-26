447557a46cc4f85e9f01faaac18ae7b7eee5d1d1
Tiffany Tam <itsteep@umich.edu>
2024-01-26 11:27:55 -0500

    ps1 JS

--- lectures/lecture-05-click-pow/script.js
+++ lectures/lecture-05-click-pow/script.js
@@ -7,20 +7,20 @@
 // already have it.
 // Then, one second later, remove the "clicked" class 
 
-const zzz = document.querySelector("#clickable_elem"); 
-const revert = () => {
-    zzz.classList.remove("clicked"); 
-}
+// const zzz = document.querySelector("#clickable_elem"); 
+// const revert = () => {
+//     zzz.classList.remove("clicked"); 
+// }
 
-const toggleImage = () => {
-    if (zzz.classList.contains("clickable")) {
-        zzz.classList.add("clicked"); 
-        setTimeout(revert, 1000); 
-        // can also do: setTimeout(() => { zzz.classList.remove("clicked")}, 1000); 
-    } 
-}; 
+// const toggleImage = () => {
+//     if (zzz.classList.contains("clickable")) {
+//         zzz.classList.add("clicked"); 
+//         setTimeout(revert, 1000); 
+//         // can also do: setTimeout(() => { zzz.classList.remove("clicked")}, 1000); 
+//     } 
+// }; 
 
-zzz.addEventListener("click", toggleImage); 
+// zzz.addEventListener("click", toggleImage); 
 
 // Sample solution: 
 // const clickableElement = document.querySelector('#clickable_elem');
@@ -32,3 +32,24 @@ zzz.addEventListener("click", toggleImage);
 //         }, 1000);
 //     }
 // });
+
+
+// The file multi_click_pow.html is similar to click_pow.html , but it has many
+// "pow" images instead of one.
+// Update your code so that when the user clicks on any of the "pow" images, it changes
+// to the "zzz" image.
+// Hint: use document.querySelectorAll() and a for loop 
+
+const clickableElements = document.querySelectorAll('.clickable');
+
+for (const clickableElement of clickableElements) {
+    clickableElement.addEventListener('click', () => {
+        if(!clickableElement.classList.contains('clicked')) {
+            clickableElement.classList.add('clicked');
+            setTimeout(() => {
+                clickableElement.classList.remove('clicked');
+            }, 1000);
+        }
+    });
+}
+
